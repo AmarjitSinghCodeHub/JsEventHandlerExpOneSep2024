0bef2ee258a146d900f58fcb1681767763c56223
AmarjitSinghCodeHub <amarjitsinghgigs@gmail.com>
2024-09-23 12:54:28 +0530

    Fifth line added to Dev Branch

--- scripts/JsScript.js
+++ scripts/JsScript.js
@@ -3,6 +3,7 @@ let x=()=>{
     alert("new line added.");
     alert("third line added.");
     alert("test for dev branch");
+    alert("new line added to branch dev new - 5 ")
 }
 
 let btn=document.querySelector("Button");
